490970c3c8ed8363493df2e4203c91e4ed72633a
Lorin Beer <lorin.beer@nitobi.com>
2012-05-09 14:05:01 -0700

    art and js

---
+++ assets/penderdemo.js
@@ -0,0 +1,88 @@
+var Log = Packages.android.util.Log;
+var Image = Packages.com.pender.glaid.Image;
+var Math = Packages.java.lang.Math;
+
+Log.d("Renderer", "onSurfaceCreated, registering gl object");
+
+//==============================================================================
+
+var console = {
+        log : function (msg) {
+            Log.d("PenderLog", msg);
+        }
+    };
+
+//==============================================================================
+//==============================================================================
+
+
+function Cat() {
+
+    this.frame = 0;
+
+    //this.xspeed = 0.01 * Math.floor(Math.random()*5);
+    //this.yspeed = 0.01 * Math.floor(Math.random()*5);
+
+    this.xpos = 50;
+    this.ypos = 50;
+
+    //this.rotation = 0.0;
+
+    //this.scalefactor = 0.5;
+
+    this.drawme = function () {
+
+        if (this.frame > 10) { this.frame = 0; }
+
+    //    this.xpos += this.xspeed;
+    //    this.ypos += this.yspeed;
+    //    if (this.xpos < -1.0 || this.xpos > 1.0) {
+    //        this.xspeed *= -1;
+    //    }
+    //    if (this.ypos < -1.0 || this.ypos > 1.0) {
+    //        this.yspeed *= -1;
+    //    }
+
+        this.frame += 1;
+      //  Canvas.scale(0.5, 0.5);
+        Canvas.translate(this.xpos, this.ypos);
+
+        Canvas.drawImage(this.frame, 0, 0);
+
+    };
+
+}
+
+//==============================================================================
+
+var cats = [];
+var catnumb = 8;
+
+//==============================================================================
+
+function init() {
+
+    var i = 0;
+
+    for (i = 0; i < catnumb; i += 1) {
+        console.log("it's a new cat!");
+        cats[i] = new Cat();
+    }
+
+}
+
+//==============================================================================
+
+function draw() {
+
+    var i = 0;
+
+    for (i = 0; i < catnumb; i += 1) {
+
+        cats[i].drawme();
+
+    }
+
+}
+
+//==============================================================================
