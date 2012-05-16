fdc329f78dd43101497993061142729e236da8f4
Lorin Beer <lorin.beer@nitobi.com>
2012-05-16 11:00:40 -0700

    update to shim, working interthread asset loading

--- assets/penderandroidshim.js
+++ assets/penderandroidshim.js
@@ -2,10 +2,9 @@ var Log = Packages.android.util.Log;
 var Image = Packages.com.pender.glaid.Image;
 var Math = Packages.java.lang.Math;
 
-
 var console = {
         log : function (msg) {
             Log.d("PenderLog", msg);
         }
-    };
+};
 
