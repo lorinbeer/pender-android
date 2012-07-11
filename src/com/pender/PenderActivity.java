a6ab736942e7a117a2f150801fa0c823aede13f7
Lorin Beer <lorin.beer@nitobi.com>
2012-07-11 12:03:39 -0700

    updated render architechture, now accepts one time and repeating js function registration

--- src/com/pender/PenderActivity.java
+++ src/com/pender/PenderActivity.java
@@ -18,12 +18,9 @@
 package com.pender;
 
 import java.io.BufferedReader;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-
 import android.app.Activity;
 import android.content.res.AssetManager;
 import android.os.Bundle;
@@ -39,82 +36,68 @@ public class PenderActivity extends Activity {
 
         mHandler = new PenderMessageHandler(this);
 
-        mGLView = new PenderView( mHandler );
-
+        mGLView = new PenderView(mHandler);
         initView();
-        
+        /*
         ArrayList<String> scripts = new ArrayList<String>();
-        
         // specify location of the shim relative to Assets directory
-        scripts.add(readJS("penderandroidshim.js") );
-        
+        scripts.add(readJS("penderandroidshim.js"));
         // specify location of js implementation of client app
         scripts.add(readJS("demos/client/penderdemo.js") );
-        
         // pin an init call to the end, calls default init(), unless redefined
         scripts.add("init();");
-        
         // execute the scripts in order specified
         ((PenderView)mGLView).execScripts(scripts);
+        */
+        ArrayList<String> scripts = new ArrayList<String>();
+        scripts.add(readJS("penderandroidshim.js"));
+      /*scripts.add(readJS("www/sunspider/sunspider-test-contents.js"));
+        scripts.add(readJS("www/sunspider/sunspider-test-prefix.js"));
+        scripts.add(readJS("www/sunspider/sunspider.js"));
+        scripts.add("start();");*/
 
+        scripts.add(readJS("demos/client/penderdemo.js"));
+        scripts.add("init();");
+        
+        ((PenderView)mGLView).execScripts(scripts);
     }
 
     public PenderView getView() { return (PenderView)this.mGLView; }
 
     private String readJS( String jspath) {		
-
-    	 BufferedReader reader = null;
-
-    	 StringBuffer script = new StringBuffer();
-
+    	BufferedReader reader = null;
+    	StringBuffer script = new StringBuffer();
     	try {
-
 	  	    AssetManager al = this.getAssets(); //sharpshooting hulk
 
 	  	    reader = new BufferedReader( 
 	  	    					new InputStreamReader( 
-	  	    						al.open(jspath) ) );
+	  	    						al.open(jspath)));
 	  	    String buf = "";
-	  	    while( ( buf = reader.readLine() ) != null ) {
-	  	    	
+	  	    while(( buf = reader.readLine()) != null) {
 	  	    	script.append("\n");
-	  	    	script.append( buf );
-	  	    	
+	  	    	script.append(buf);
 	  	    }
-	  	    	
   	    } catch (IOException e ) {
-  	    	
   	    	Log.d("exception", e.toString() );
   	    	Log.d("handle this gracefully","wouldn't you?");
-  	    	
   	    }
-
     	return script.toString();
-
     }
 
     private void initView() {
-
         final Activity ctx = this;
-
     	this.runOnUiThread( new Runnable() {
     	                        public void run() { 
-    	                         
     	                              RelativeLayout top = new RelativeLayout( ctx );
-    	                              top.setLayoutParams( new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.FILL_PARENT, 
-    	                                                                                    RelativeLayout.LayoutParams.FILL_PARENT ) );
-    	                 
+    	                              top.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, 
+    	                                                                                  RelativeLayout.LayoutParams.FILL_PARENT));
     	                              top.addView( mGLView );
-    	      
     	                              ctx.setContentView( top );
-    	      
     	                          } //end run
     	}); // end runnable
     } //initView
 
-
     private GLSurfaceView mGLView;
-
     private PenderMessageHandler mHandler;
-    
 }
